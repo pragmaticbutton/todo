3f8a61bcde9550285244cb754fa4e30c47b17ecc
pragmaticbutton <jelenaziv96@gmail.com>
2025-09-21 14:44:20 +0200

    rename memory to lowercase since it shouldn't be exported

--- internal/storage/memory/memory.go
+++ internal/storage/memory/memory.go
@@ -7,19 +7,19 @@ import (
 	"todo/internal/domain/task"
 )
 
-type Memory struct {
+type memory struct {
 	tasks map[uint32]*task.Task
 	lists map[uint32]*list.List
 }
 
-var data *Memory // TODO: is singleton really needed here? Also, should this be protected by mutex or sync.Map?
+var data *memory // TODO: is singleton really needed here? Also, should this be protected by mutex or sync.Map?
 
 var once sync.Once
 
-func New() *Memory {
+func New() *memory {
 	if data == nil {
 		once.Do(func() {
-			data = &Memory{
+			data = &memory{
 				tasks: make(map[uint32]*task.Task),
 				lists: make(map[uint32]*list.List),
 			}
@@ -29,12 +29,12 @@ func New() *Memory {
 	return data
 }
 
-func (m *Memory) AddTask(t *task.Task) error {
+func (m *memory) AddTask(t *task.Task) error {
 	m.tasks[t.ID] = t
 	return nil
 }
 
-func (m *Memory) ListTasks() ([]task.Task, error) {
+func (m *memory) ListTasks() ([]task.Task, error) {
 	ts := make([]task.Task, 0, len(m.tasks))
 	for _, t := range m.tasks {
 		ts = append(ts, *t)
@@ -43,7 +43,7 @@ func (m *Memory) ListTasks() ([]task.Task, error) {
 	return ts, nil
 }
 
-func (m *Memory) GetTask(id uint32) (*task.Task, error) {
+func (m *memory) GetTask(id uint32) (*task.Task, error) {
 	t, ok := m.tasks[id]
 	if !ok {
 		return nil, fmt.Errorf("task with id %d not found", id)
@@ -51,7 +51,7 @@ func (m *Memory) GetTask(id uint32) (*task.Task, error) {
 	return t, nil
 }
 
-func (m *Memory) DeleteTask(id uint32) error {
+func (m *memory) DeleteTask(id uint32) error {
 	_, ok := m.tasks[id]
 	if !ok {
 		return fmt.Errorf("task with id %d not found", id)
@@ -60,7 +60,7 @@ func (m *Memory) DeleteTask(id uint32) error {
 	return nil
 }
 
-func (m *Memory) UpdateTask(t *task.Task) error {
+func (m *memory) UpdateTask(t *task.Task) error {
 	if _, ok := m.tasks[t.ID]; !ok {
 		return fmt.Errorf("task with id %d not found", t.ID)
 	}
@@ -68,16 +68,16 @@ func (m *Memory) UpdateTask(t *task.Task) error {
 	return nil
 }
 
-func (m *Memory) NextTaskID() uint32 {
+func (m *memory) NextTaskID() uint32 {
 	return uint32(len(m.tasks) + 1)
 }
 
-func (m *Memory) AddList(l *list.List) error {
+func (m *memory) AddList(l *list.List) error {
 	m.lists[l.ID] = l
 	return nil
 }
 
-func (m *Memory) ListLists() ([]list.List, error) {
+func (m *memory) ListLists() ([]list.List, error) {
 	ls := make([]list.List, 0, len(m.lists))
 	for _, l := range m.lists {
 		ls = append(ls, *l)
@@ -86,7 +86,7 @@ func (m *Memory) ListLists() ([]list.List, error) {
 	return ls, nil
 }
 
-func (m *Memory) GetList(id uint32) (*list.List, error) {
+func (m *memory) GetList(id uint32) (*list.List, error) {
 	l, ok := m.lists[id]
 	if !ok {
 		return nil, fmt.Errorf("list with id %d not found", id)
@@ -94,7 +94,7 @@ func (m *Memory) GetList(id uint32) (*list.List, error) {
 	return l, nil
 }
 
-func (m *Memory) DeleteList(id uint32) error {
+func (m *memory) DeleteList(id uint32) error {
 	_, ok := m.lists[id]
 	if !ok {
 		return fmt.Errorf("list with id %d not found", id)
@@ -103,7 +103,7 @@ func (m *Memory) DeleteList(id uint32) error {
 	return nil
 }
 
-func (m *Memory) UpdateList(l *list.List) error {
+func (m *memory) UpdateList(l *list.List) error {
 	if _, ok := m.lists[l.ID]; !ok {
 		return fmt.Errorf("list with id %d not found", l.ID)
 	}
@@ -111,11 +111,11 @@ func (m *Memory) UpdateList(l *list.List) error {
 	return nil
 }
 
-func (m *Memory) NextListID() uint32 {
+func (m *memory) NextListID() uint32 {
 	return uint32(len(m.lists) + 1)
 }
 
-func (m *Memory) SearchTasks(listID *uint32) ([]task.Task, error) {
+func (m *memory) SearchTasks(listID *uint32) ([]task.Task, error) {
 	ts := []task.Task{}
 
 	for _, t := range m.tasks {
