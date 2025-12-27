7884e19e2ce7551bb4b733c4952520b8a74f7812
pragmaticbutton <jelenaziv96@gmail.com>
2025-12-27 11:55:39 +0100

    add basic interactive mode

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	"github.com/pragmaticbutton/todo/internal/cli/interactive"
 	"github.com/pragmaticbutton/todo/internal/cli/list"
 	"github.com/pragmaticbutton/todo/internal/cli/task"
 	"github.com/pragmaticbutton/todo/internal/service"
@@ -8,14 +9,29 @@ import (
 )
 
 func NewRootCmd(taskService *service.TaskService, listService *service.ListService) *cobra.Command {
+
+	var interactiveMode bool
+
 	cmd := &cobra.Command{
-		Use:           "todo",
-		Short:         "My TODO application",
-		Long:          `The best TODO application in the world.`,
-		SilenceUsage:  true,
-		SilenceErrors: true,
-		// Attach top-level groups here.
+		Use:   "todo",
+		Short: "My TODO application",
+		Long:  `The best TODO application in the world.`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if interactiveMode {
+				return interactive.Run(cmd)
+			}
+
+			return cmd.Help()
+		},
 	}
+
+	cmd.Flags().BoolVarP(
+		&interactiveMode,
+		"interactive",
+		"i",
+		false,
+		"Run in interactive mode",
+	)
 
 	cmd.AddCommand(task.NewTaskCmd(taskService))
 	cmd.AddCommand(list.NewListCmd(listService))
